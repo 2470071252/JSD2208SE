da8a4bcf1b877e7deea3f3109e66370257482a64
Masterz <2470071252@qq.com>
2022-10-11 10:34:40 +0800

    2022/10/10 新增同步块知识

--- src/main/java/thread/SyncDemo2.java
+++ src/main/java/thread/SyncDemo2.java
@@ -12,13 +12,43 @@ package thread;
  */
 public class SyncDemo2 {
     public static void main(String[] args) {
+        shop shop = new shop();
+        Thread t0 = new Thread("人物1"){
+            @Override
+            public void run() {
+                shop.buy();
+            }
+        };
 
+        Thread t1 = new Thread("人物2"){
+            @Override
+            public void run() {
+                shop.buy();
+            }
+        };
+        t0.start();
+        t1.start();
     }
 }
 
 class shop{
     public void buy(){
+        try {
+            Thread t = Thread.currentThread();
+            System.out.println(t.getName()+"：正在挑衣服");
+            Thread.sleep(5000);
 
-        System.out.println("");
+            /*
+            同步块可以更精准的锁定需要多个线程
+             */
+            synchronized (this){
+                System.out.println(t.getName()+"：正在试衣间试衣服");
+                Thread.sleep(5000);
+            }
+
+            System.out.println(t.getName()+"：结账离开");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
