5cd7863966a09615e29570d764cd2cb85178b5a4
Masterz <2470071252@qq.com>
2022-10-11 16:31:11 +0800

    2022/10/11 Socket改进

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -2,8 +2,9 @@ package socket;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+
+import static java.nio.charset.StandardCharsets.*;
 
 /**
  * 聊天室客户端
@@ -15,6 +16,7 @@ public class Client {
     远端计算机建立连接并基于两条流的IO操作完成与服务端的数据交互
      */
     private Socket socket;
+
     public Client() {
         /*
         实例化Socket时就是与服务端建立连接的过程，这里需要传入两个参数
@@ -23,31 +25,38 @@ public class Client {
          */
         try {
             System.out.println("Loading......");
-            socket = new Socket("127.0.0.1", 8000);
+            socket = new Socket("localhost", 8000);
             System.out.println("successfully！");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void start(){
-        try {
-            // 低级流，作用：将写出的字节发送给建立连接的远端计算机
-            OutputStream out = socket.getOutputStream();
+    public void start() {
+        try (
+                // 低级流，作用：将写出的字节发送给建立连接的远端计算机
+                OutputStream out = socket.getOutputStream();
+        ) {
+            // 启动用来读取来自服务端发送过来消息的线程
+            ServerHandler han = new ServerHandler();
+            Thread t = new Thread(han);
+            t.start();
             // 转换流(高级流)，作用1.衔接字节流与字符流 2.将写出的字符转换为字节
-            OutputStreamWriter osw = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            OutputStreamWriter osw = new OutputStreamWriter(out, UTF_8);
             // 缓冲流(高级流)，作用：块写文本数据加速
             BufferedWriter bw = new BufferedWriter(osw);
             // PrintWriter(高级流)， 作用：1.按行写出字符串 2.自动刷新
-            PrintWriter pw = new PrintWriter(bw,true);
+            PrintWriter pw = new PrintWriter(bw, true);
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("请输入文字：");
-            while (true){
+            while (true) {
                 String message = scanner.nextLine();
                 if ("exit".equals(message.toLowerCase())) {
                     break;
                 }
                 pw.println(message);
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -58,5 +67,29 @@ public class Client {
     public static void main(String[] args) {
         Client client = new Client();
         client.start();
+    }
+
+    /**
+     * 该线程任务负责读取来自服务端发送过来的消息
+     */
+    private class ServerHandler implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                // 通过socket获取输入流，来读取远端计算机(服务端)发送过来的消息
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is, UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+                String message;
+                while ((message = br.readLine())!=null){
+                    // 读取服务端发送过来的消息
+                    System.out.println(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -1,12 +1,12 @@
 package socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.*;
 
 /**
  * 聊天室服务端
@@ -19,6 +19,11 @@ public class Server {
      * 2.侦听端口，等待客户端连接
      */
     private ServerSocket serverSocket;
+    // 用来保存所有客户端的输出流
+    private List<PrintWriter> allOut = new ArrayList<>();
+    private List<Socket> allSocket = new ArrayList<>();
+
+
     public Server() {
         try {
             System.out.println("Server Starting......");
@@ -29,10 +34,10 @@ public class Server {
         }
 
     }
-    public void start(){
+
+    public void start() {
         try {
-            while (true){
-                System.out.println("Wait client connection!");
+            while (true) {
                 Socket socket = serverSocket.accept(); // serverSocket.accept()的返回值是Socket类型
                 System.out.println("A client connected.");
                 // 启动一个线程来处理与该客户端的交互
@@ -56,8 +61,9 @@ public class Server {
     /**
      * 该线程任务负责与指定的客户端交互
      */
-    private class ClientHandler implements Runnable{
+    private class ClientHandler implements Runnable {
         private Socket socket;
+
         public ClientHandler(Socket socket) {
             this.socket = socket;
         }
@@ -67,12 +73,41 @@ public class Server {
             try {
                 // 通过socket获取输入流读取自远端计算机发送过来的数据
                 InputStream in = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(in, StandardCharsets.UTF_8);
+                InputStreamReader isr = new InputStreamReader(in, UTF_8);
                 BufferedReader br = new BufferedReader(isr);
+
+                // 通过socket获取输出流，用来将消息发发送给（远端计算机）客户端
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os, UTF_8);
+                BufferedWriter bw = new BufferedWriter(osw);
+                PrintWriter pw = new PrintWriter(bw, true);
+                // 将该输出流存入共享集合
+                allOut.add(pw);
+                allSocket.add(socket);
+
                 // 读取一行来自远端计算机发送过来的字符串
                 String fromClientMessages;
-                while ((fromClientMessages =br.readLine())!=null){
-                    System.out.println("来自客户端"+socket.getInetAddress()+"的消息： "+fromClientMessages);
+                /*
+                当我调用br.readLine()读取来自远端计算机发送过来的一行字符串时，
+                远端计算机如果调用了socket.close()正常与我们断开连接，那么
+                readLine()方法会返回null.
+                如果客户端是异常中断(比如强制杀死进程)，这里readLine()方法会
+                抛出异常：java.net.SocketException: Connection reset
+                 */
+                while ((fromClientMessages = br.readLine()) != null) {
+                    System.out.println("来自客户端" + socket.getInetAddress() + "的消息： " + fromClientMessages);
+                    // 将消息广播给所有客户端
+                    /*
+                    for ( PrintWriter o : allOut ) {
+                        o.println("客户端说：" + fromClientMessages);
+                    }
+                    */
+                    for ( PrintWriter o : allOut ) {
+//                        o.println("客户端说："+fromClientMessages);
+                        for ( Socket so : allSocket ) {
+                            o.println("来自客户端"+so.getInetAddress()+"的消息： "+fromClientMessages);
+                        }
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
